5c0ec48ba5c6fbbc8c0676d22319c16b1eaf47f6
Christian Garcia <158063019+christiangarcia0311@users.noreply.github.com>
2024-09-06 11:21:26 +0800

    Add email sender test using JS

---
+++ APITest/Sender_API.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+async function sendEmail(reciever) {
+    const API_URL = "https://emailsender000.pythonanywhere.com/send-email";
+
+    const data = {
+        email_reciever: reciever,
+        msg_subject: 'Email Test',
+        msg_body: 'This is the sample message using EmailSender API.',
+        sender_name: 'EmailSender'
+    };
+
+    try {
+        const response = await axios.post(API_URL, data, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log('success: ', response.data);
+    }
+    catch (error) {
+        console.log('error: ', error.response ? error.response.data : error.message);  
+    }
+}
+
+sendEmail('christiang03112003@gmail.com');
